199c4fcd654ee1951b24a247abb3c798b4fd2408
Laercio-Note <lndsilva@hotmail.com>
2021-04-22 11:02:15 -0300

    Initial commit

---
+++ app/src/main/java/br/com/local/appempresati97/MenuPrincipal_Activity.java
@@ -0,0 +1,14 @@
+package br.com.local.appempresati97;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuPrincipal_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_principal_layout);
+    }
+}
---
+++ app/src/main/java/br/com/local/appempresati97/PerfilUsuario_Activity.java
@@ -0,0 +1,14 @@
+package br.com.local.appempresati97;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PerfilUsuario_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.perfil_usuario_layout);
+    }
+}
