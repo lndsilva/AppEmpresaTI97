f2c68353728fe149329d9e91e5f4afd2bbef027d
lndsilva <lndsilva@hotmail.com>
2021-05-10 11:00:00 -0300

    botão sair

--- app/src/main/java/br/com/local/appempresati97/Login_Activity.java
+++ app/src/main/java/br/com/local/appempresati97/Login_Activity.java
@@ -3,12 +3,35 @@ package br.com.local.appempresati97;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login_Activity extends AppCompatActivity {
+    //Declarando variável global
+    Button btnSair;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        //Apresentar o componente XML ao componente Java
+
+        btnSair = findViewById(R.id.btnSair);
+
+        //Realizar a ação de clique no btnSair através do Java
+
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Método para fechar a janela
+                finish();
+
+            }
+        });
+
+
     }
 }
